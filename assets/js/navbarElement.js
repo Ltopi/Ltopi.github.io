4b8210424a3171dabccbaf58fa370599617ebfbf
Vasilis <hadjipanosbill@gmail.com>
2024-03-11 15:53:48 +0100

    testings stuff

--- assets/js/navbarElement.js
+++ assets/js/navbarElement.js
@@ -63,62 +63,62 @@ navbar.innerHTML = `
 `
 
 class Navbar extends HTMLElement {
-    constructor() {
-        super()
-        this.attachShadow({ mode: 'open' })
-        this.shadowRoot.appendChild(navbar.content.cloneNode(true))
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(navbar.content.cloneNode(true))
+  }
+
+  connectedCallback() {
+
+    const props = {
+      active: this.getAttribute('active'),
+      breakpoint: this.getAttribute('breakpoint')
     }
 
-    connectedCallback() {
+    const btn = new NavbarBtn(props.breakpoint)
+    this.shadowRoot.querySelector('header div').append(btn)
 
-        const props = {
-            active: this.getAttribute('active'),
-            breakpoint: this.getAttribute('breakpoint')
-        }
-
-        const btn = new NavbarBtn(props.breakpoint)
-        this.shadowRoot.querySelector('header div').append(btn)
-
-        const data = {
-            header: this.shadowRoot.querySelector('header'),
-            button: this.shadowRoot.querySelector('header navbar-btn'),
-            nav: this.shadowRoot.querySelector('header nav'),
-            links: document.querySelectorAll('navigation-bar a'),
-        }
-
-        const dist = data.nav.offsetHeight - 32
-
-        this.checkIfMobile(data.nav, props.breakpoint, dist)
-
-        this.createStyle(data.header.offsetHeight)
-        this.breakpointStyles(props.breakpoint, props.position)
-
-        this.setActiveLink(data.links, props.active)
-        data.button.addEventListener('click', () => {
-            data.nav.classList.toggle('show')
-        })
-        window.addEventListener('resize', () => { this.checkIfMobile(data.nav, props.breakpoint, dist) })
+    const data = {
+      header: this.shadowRoot.querySelector('header'),
+      button: this.shadowRoot.querySelector('header navbar-btn'),
+      nav: this.shadowRoot.querySelector('header nav'),
+      links: document.querySelectorAll('navigation-bar a'),
     }
 
-    disconnectedCallback() {
-        window.removeEventListener('resize', this.checkIfMobile)
-    }
+    const dist = data.nav.offsetHeight - 32
 
-    setActiveLink(elements, attr) {
-        elements.forEach((link) => {
-            if (link.innerHTML.toLowerCase() === attr.toLowerCase()) {
-                link.classList.add('nav-link--active')
-            }
-        })
-    }
+    this.checkIfMobile(data.nav, props.breakpoint, dist)
 
-    createStyle(distance) {
-        const styles = `.show { transform: translateY(${distance}px) !important; }`;
-        this.shadowRoot.querySelector('style').append(styles)
-    }
+    this.createStyle(data.header.offsetHeight)
+    this.breakpointStyles(props.breakpoint, props.position)
 
-    breakpointStyles(breakpoint) {
-        const styles = `
+    this.setActiveLink(data.links, props.active)
+    data.button.addEventListener('click', () => {
+      data.nav.classList.toggle('show')
+    })
+    window.addEventListener('resize', () => { this.checkIfMobile(data.nav, props.breakpoint, dist) })
+  }
+
+  disconnectedCallback() {
+    window.removeEventListener('resize', this.checkIfMobile)
+  }
+
+  setActiveLink(elements, attr) {
+    elements.forEach((link) => {
+      if (link.innerHTML.toLowerCase() === attr.toLowerCase()) {
+        link.classList.add('nav-link--active')
+      }
+    })
+  }
+
+  createStyle(distance) {
+    const styles = `.show { transform: translateY(${distance}px) !important; }`;
+    this.shadowRoot.querySelector('style').append(styles)
+  }
+
+  breakpointStyles(breakpoint) {
+    const styles = `
     @media screen and (min-width: ${breakpoint}px) {
       header {
         box-shadow: var(--navbar-shadow);
@@ -141,25 +141,24 @@ class Navbar extends HTMLElement {
       }
     }
     `
-        this.shadowRoot.querySelector('style').append(styles)
-    }
+    this.shadowRoot.querySelector('style').append(styles)
+  }
 
-    calTransDistance(nav) {
-        return nav.offsetHeight
-    }
+  calTransDistance(nav) {
+    return nav.offsetHeight
+  }
 
-    checkIfMobile(nav, breakpoint, distance) {
-        if (window.innerWidth < breakpoint) {
-            nav.style.transform = `translateY(-${distance}px)`
-            console.info("here")
-            setTimeout(() => {
-                nav.classList.add('animate')
-            }, 1)
-        } else {
-            nav.classList.remove('show')
-            nav.classList.remove('animate')
-        }
+  checkIfMobile(nav, breakpoint, distance) {
+    if (window.innerWidth < breakpoint) {
+      nav.style.transform = `translateY(-${distance}px)`
+      setTimeout(() => {
+        nav.classList.add('animate')
+      }, 1)
+    } else {
+      nav.classList.remove('show')
+      nav.classList.remove('animate')
     }
+  }
 }
 
 const navbarBtn = document.createElement('template')
@@ -210,32 +209,32 @@ navbarBtn.innerHTML = `
 `
 
 class NavbarBtn extends HTMLElement {
-    constructor(breakpoint) {
-        super()
-        this.attachShadow({ mode: 'open' })
-        this.shadowRoot.appendChild(navbarBtn.content.cloneNode(true))
-        this.breakpoint = breakpoint
+  constructor(breakpoint) {
+    super()
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(navbarBtn.content.cloneNode(true))
+    this.breakpoint = breakpoint
+  }
+
+  connectedCallback() {
+    const btn = this.shadowRoot.querySelector('button')
+
+    btn.addEventListener('click', () => {
+      btn.classList.toggle('change')
+    })
+
+    window.addEventListener('resize', () => { this.reset(btn, this.breakpoint) })
+  }
+
+  disconnectedCallback() {
+    window.removeEventListener('resize', this.reset)
+  }
+
+  reset(element, breakpoint) {
+    if (window.innerWidth > breakpoint && element.classList.contains('change')) {
+      element.classList.remove('change')
     }
-
-    connectedCallback() {
-        const btn = this.shadowRoot.querySelector('button')
-
-        btn.addEventListener('click', () => {
-            btn.classList.toggle('change')
-        })
-
-        window.addEventListener('resize', () => { this.reset(btn, this.breakpoint) })
-    }
-
-    disconnectedCallback() {
-        window.removeEventListener('resize', this.reset)
-    }
-
-    reset(element, breakpoint) {
-        if (window.innerWidth > breakpoint && element.classList.contains('change')) {
-            element.classList.remove('change')
-        }
-    }
+  }
 }
 
 window.customElements.define('navbar-btn', NavbarBtn)
